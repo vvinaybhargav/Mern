4c2623000933f833f36e1195a99a2f35617b216d
vinay <vvinaybhargav@gmail.com>
2021-03-16 22:05:47 +0530

    mongodberrorfixed

--- config/db.js
+++ config/db.js
@@ -4,13 +4,16 @@ const db = config.get("mongoURI");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(db, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(
+      "mongodb://mongodb:mongodb@cluster0-shard-00-00.6cviq.mongodb.net:27017,cluster0-shard-00-01.6cviq.mongodb.net:27017,cluster0-shard-00-02.6cviq.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-bc7d8c-shard-0&authSource=admin&retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
     console.log("Mongo DB Connected......");
-  } catch {
-    console.log("Not Connected");
+  } catch (err) {
+    console.log(err.message);
     process.exit(1);
   }
 };
